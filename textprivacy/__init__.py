74a103c55bd44fb6d73bf04bd5bc4f66d9af8aaf
thearol <thearollesloth@hotmail.com>
2024-09-18 13:40:46 +0200

    change init paths to match fork

--- textprivacy/__init__.py
+++ textprivacy/__init__.py
@@ -3,6 +3,9 @@
 __author__ = """Martin Closter Jespersen"""
 __email__ = "martincjespersen@gmail.com"
 __version__ = "0.1.0"
-from textprivacy import utils
-from textprivacy.textanonymization import TextAnonymizer
-from textprivacy.textpseudonymization import TextPseudonymizer
+#from textprivacy import utils
+#from textprivacy.textanonymization import TextAnonymizer
+#from textprivacy.textpseudonymization import TextPseudonymizer
+from . import utils
+from .textanonymization import TextAnonymizer
+from .textpseudonymization import TextPseudonymizer
